c9245005cf7660e1e34d8b9846fe903f8bf40bc3
Adam ACHBAROU <aachbaro@e3r3p16.42.fr>
2021-01-21 12:39:04 +0100

    j'ai l'idee

--- test/get_next_line.c
+++ test/get_next_line.c
@@ -17,21 +17,65 @@
 #include <string.h>
 #include <stdio.h>
 
-
-
 int	get_next_line(int fd, char **line)
 {
+	int		size;
+	char	buf[2];
+	char	*lines;
+	char	*tmp;
+	int		ret;
+	int		i;
 
+	lines = malloc(sizeof(char));
+	lines[0] = 0;
+	tmp = malloc(sizeof(char) * 2);
+	size = 0;
+	ret = read(fd, tmp, 1);
+	tmp[1] = 0;
+	while (buf[0] != '\n' && ret)
+	{
+		free(lines);
+		i = 0;
+		lines = malloc(sizeof(char) * (size + 2));
+		while (tmp[i])
+		{
+			lines[i] = tmp[i];
+			i++;
+		}
+		lines[i] = 0;
+		size++;
+		free(tmp);
+		ret = read(fd, buf, 1);
+		buf[1] = 0;
+		tmp = malloc(sizeof(char) * (size + 2));
+		i = 0;
+		while (lines[i])
+		{
+			tmp[i] = lines[i];
+			i++;
+		}
+		tmp[i++] = buf[0];
+		tmp[i] = 0;
+//		printf("\n\n-----\n%s\n", lines);
+	}
+	*line = lines;
 	return (0);
 }
 
 int	main(void)
 {
 	int		fd;
-	char	*line;
+	char	**lines;
+	int		i;
 
+	i = 0;
+	lines = malloc(sizeof(char *) * 10);
 	fd = open("text", O_RDONLY);
-	get_next_line(fd, &line);
-	printf("\n %s", line);
+	while (i < 10)
+	{
+		get_next_line(fd, &lines[i]);
+		printf("%s\n", lines[i]);
+		i++;
+	}
 	return (0);
 }
--- test/get_next_line.h
+++ test/get_next_line.h
@@ -1,37 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aachbaro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/19 13:39:14 by aachbaro          #+#    #+#             */
-/*   Updated: 2021/01/20 15:27:38 by aachbaro         ###   ########.fr       */
+/*   Created: 2021/01/19 13:39:42 by aachbaro          #+#    #+#             */
+/*   Updated: 2021/01/19 15:28:31 by aachbaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <string.h>
-#include <stdio.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <unistd.h>
+# include <stdlib.h>
+# define BUFFER_SIZE 4096
 
-
-int	get_next_line(int fd, char **line)
-{
-
-	return (0);
-}
-
-int	main(void)
-{
-	int		fd;
-	char	*line;
-
-	fd = open("text", O_RDONLY);
-	get_next_line(fd, &line);
-	printf("\n %s", line);
-	return (0);
-}
+#endif
